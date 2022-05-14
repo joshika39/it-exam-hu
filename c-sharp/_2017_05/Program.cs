bf8be54940f7814104a4464c5025897650863dd8
Joshua Hegedus <jhegedus9@gmail.com>
2022-05-14 21:16:44 +0200

    Finally done

--- c-sharp/_2017_05/Program.cs
+++ c-sharp/_2017_05/Program.cs
@@ -1,165 +1,144 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
-namespace _2017_05_idegen
+namespace _2017_05
 {
-    struct Furdozo
-    {
-        public int id;
-        public int reszlegid;
-        public int beki;
-        public int ora;
-        public int perc;
-        public int mp;
-        public int ido;
-
-        public Furdozo(string sor) : this()
-        {
-            var adatok = sor.Split(' ');
-            id = int.Parse(adatok[0]);
-            reszlegid = int.Parse(adatok[1]);
-            beki = int.Parse(adatok[2]);
-            ora = int.Parse(adatok[3]);
-            perc = int.Parse(adatok[4]);
-            mp = int.Parse(adatok[5]);
-            ido = mp + perc * 60 + (ora * 60 * 60);
-        }
-    }
-
     internal class Program
     {
+
+        private const string _utvonal = "../../../valaszok.txt";
+        private static string _valasz;
+        private static List<Versenyzo> _versenyzok;
+        class Versenyzo
+        {
+            public string Id { get; set; }
+            public string Valaszok { get; set; }
+            public int Pont { get; set; }
+
+            public Versenyzo(string id, string valaszok)
+            {
+                Id = id;
+                Valaszok = valaszok;
+                Pont = 0;
+                for (int i = 0; i < Valaszok.Length; i++)
+                {
+                    if(i >= 0 && i <= 4)
+                    {
+                        if (Valaszok[i] == _valasz[i]) Pont += 3;
+                    }
+                    else if (i >= 5 && i <= 9)
+                    {
+                        if (Valaszok[i] == _valasz[i]) Pont += 4;
+
+                    }
+                    else if (i >= 10 && i <= 12)
+                    {
+                        if (Valaszok[i] == _valasz[i]) Pont += 5;
+                    }
+                    else if (i == 13)
+                    {
+                        if (Valaszok[i] == _valasz[i]) Pont += 6;
+                    }
+                }
+            }
+        }
+
         static void Main(string[] args)
         {
-            var furdozok = new List<Furdozo>();
-
-            var sorok = File.ReadLines("../../../furdoadat.txt");
-
-            foreach (var sor in sorok)
-            {
-                var ideiglenesFurdozo = new Furdozo(sor);
-                furdozok.Add(ideiglenesFurdozo);
-            }
-
-
-            Console.WriteLine("2. feladat");
-            Console.WriteLine($"Az első vendég {furdozok[0].ora}:{furdozok[0].perc}:{furdozok[0].mp}-kor lépett ki az öltözőből.");
-
-
-            var ora = 0;
-            var perc = 0;
-            var mp = 0;
-
-            //furdozok[i].beki == 1 && furdozok[i].reszlegid == 0
-            for (int i = 0; i < furdozok.Count; i++)
-            {
-                if (furdozok[i].ora >= ora && furdozok[i].beki == 1 && furdozok[i].reszlegid == 0)
-                {
-                    ora = furdozok[i].ora;
-                }
-
-            }
-            for (int i = 0; i < furdozok.Count; i++)
-            {
-                if (furdozok[i].perc >= perc && furdozok[i].ora == ora && furdozok[i].beki == 1 && furdozok[i].reszlegid == 0)
-                {
-                    perc = furdozok[i].perc;
-                }
-
-            }
-            for (int i = 0; i < furdozok.Count; i++)
-            {
-                if (furdozok[i].mp >= mp && furdozok[i].perc == perc && furdozok[i].ora == ora && furdozok[i].beki == 1 && furdozok[i].reszlegid == 0)
-                {
-                    mp = furdozok[i].mp;
-                }
-
-            }
-            Console.WriteLine($"Az utolsó vendég {ora}:{perc}:{mp}-kor lépett ki az öltözőből.");
-
-            Console.WriteLine("\n3. feladat");
-            var egyreszenjart = 0;
-            var tomb = new int[1000];
-            for (int i = 0; i < tomb.Length; i++)
-            {
-                tomb[i] = 0;
-            }
-
-            for (int i = 0; i < furdozok.Count; i++)
-            {
-                var szemelyek = furdozok[i].id;
-                tomb[szemelyek]++;
-            }
-
-            for (int i = 0; i < tomb.Length; i++)
-            {
-                if (tomb[i] > 0)
-                {
-                    if (tomb[i] == 4)
-                    {
-                        egyreszenjart++;
-                    }
-                }
-            }
-            Console.WriteLine($"A fürdőben {egyreszenjart} vendég járt csak egy részlegen./n");
-
-            Console.WriteLine("4. feladat");
-
-
-            var legtobbido = 0;
-            var legtobbId = 0;
-
-            var tomb2 = new int[1000];
-            for (int i = 0; i < tomb2.Length; i++)
-            {
-                tomb2[i] = 0;
-            }
-
-            var kezdes = 0;
-
-            for (int i = 0; i < furdozok.Count; i++)
-            {
-                var pillantnyiIdo = furdozok[i].ido;
-                var id = furdozok[i].id;
-                var test = kezdes;
-
-                if (furdozok[i].reszlegid == 0 && furdozok[i].beki == 1)
-                {
-                    kezdes = pillantnyiIdo;
-                }
-               
-                if (furdozok[i].reszlegid == 0 && furdozok[i].beki == 0)
-                {
-                    tomb2[id] += pillantnyiIdo - kezdes;
-                    kezdes = 0;
-                }
-
-            }
-
-            for (int i = tomb2.Length - 1; i >= 0; i--)
-            {
-                if (tomb2[i] > 0)
-                {
-                    if (tomb2[i] >= legtobbido)
-                    {
-                        legtobbido = tomb2[i];
-                        legtobbId = i;
-                    }
-                }
-            }
-            Console.WriteLine(legtobbId);
-
-            //for (int i = 0; i < furdozok.Count; i++)
-            //{
-            //    var szemely = furdozok[i];
-            //    for (int j = 0; j < furdozok.Count; j++)
-            //    {
-            //        if(szemely.id == furdozok[j].id)
-            //        {
-
-            //        }
-            //    }
-            //}
+            FeladatKiir(1, Feladat1);
+            FeladatKiir(2, Feladat2);
+            FeladatKiir(3, Feladat3);
+            FeladatKiir(5, Feladat5);
+            FeladatKiir(7, Feladat7);
         }
+
+        private static void FeladatKiir(int feladatSzam, Action feladat)
+        {
+            
+            Console.Write($"{feladatSzam}. Feladat: ");
+            feladat.DynamicInvoke();
+            Console.WriteLine("\n");
+            
+        }
+        
+        private static void Feladat1()
+        {
+            _versenyzok = new List<Versenyzo>();
+            _valasz = File.ReadLines(_utvonal).First();
+            foreach (var sor in File.ReadAllLines(_utvonal))
+            {
+                var adatok = sor.Split(' ');
+                if(adatok.Length > 1)
+                    _versenyzok.Add(new Versenyzo(adatok[0], adatok[1]));
+            }
+            Console.Write("Adatok beolvasa");
+        }
+
+        private static void Feladat2()
+        {
+            Console.Write($"A versenyen {_versenyzok.Count} versenyzo vett reszt");
+        }
+
+        private static void Feladat3()
+        {
+            Console.Write("A versenyzo azonositoja = ");
+            var id = Console.ReadLine();
+            var versenyzo = _versenyzok.Where(v => v.Id == id).FirstOrDefault();
+            Console.WriteLine($"{versenyzo.Valaszok}\t(a versenyző válasza)");
+
+            Console.WriteLine("4. Feladat:");
+            Console.WriteLine($"{_valasz}\t(a helyes megoldás)");
+            for (int i = 0; i < versenyzo.Valaszok.Length; i++)
+            {
+                char c = versenyzo.Valaszok[i];
+                if (c == _valasz[i]) Console.Write("+"); else Console.Write(" ");
+            }
+            Console.WriteLine("\t(a versenyző helyes válaszai)");
+        }
+        
+        private static void Feladat5()
+        {
+            Console.Write("A feladat sorszáma = ");
+            var sorszam = int.Parse(Console.ReadLine());
+            var joValaszok = 0;
+
+            foreach (var v in _versenyzok)
+            {
+                if(v.Valaszok[sorszam-1] == _valasz[sorszam - 1])
+                {
+                    joValaszok++;
+                }
+            }
+            double szazalek = Math.Round((double)joValaszok / (double)_versenyzok.Count * 100, 2);
+            Console.WriteLine($"A feladatra {joValaszok} fo valaszolt jol, ami a versenyzok {szazalek}%-a");
+        }
+
+        private static void Feladat7()
+        {
+            var sortedList = _versenyzok.OrderByDescending(x => x.Pont).ToList();
+
+            var hely = 1;
+            Console.WriteLine();
+            for (int i = 0; i < sortedList.Count; i++)
+            {
+                Versenyzo v = sortedList[i];
+                if(hely <= 3)
+                {
+                    if (v.Pont == sortedList[i + 1].Pont)
+                    {
+                        Console.WriteLine($"{hely}. dij ({v.Pont} pont): {v.Id}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"{hely}. dij ({v.Pont} pont): {v.Id}"); hely++;
+                    }
+                        
+                }
+                
+            }
+        }
+
     }
 }
---
+++ c-sharp/_2017_05_idegen/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace _2017_05_idegen
+{
+    struct Furdozo
+    {
+        public int id;
+        public int reszlegid;
+        public int beki;
+        public int ora;
+        public int perc;
+        public int mp;
+        public int ido;
+
+        public Furdozo(string sor) : this()
+        {
+            var adatok = sor.Split(' ');
+            id = int.Parse(adatok[0]);
+            reszlegid = int.Parse(adatok[1]);
+            beki = int.Parse(adatok[2]);
+            ora = int.Parse(adatok[3]);
+            perc = int.Parse(adatok[4]);
+            mp = int.Parse(adatok[5]);
+            ido = mp + perc * 60 + (ora * 60 * 60);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var furdozok = new List<Furdozo>();
+
+            var sorok = File.ReadLines("../../../furdoadat.txt");
+
+            foreach (var sor in sorok)
+            {
+                var ideiglenesFurdozo = new Furdozo(sor);
+                furdozok.Add(ideiglenesFurdozo);
+            }
+
+
+            Console.WriteLine("2. feladat");
+            Console.WriteLine($"Az első vendég {furdozok[0].ora}:{furdozok[0].perc}:{furdozok[0].mp}-kor lépett ki az öltözőből.");
+
+
+            var ora = 0;
+            var perc = 0;
+            var mp = 0;
+
+            //furdozok[i].beki == 1 && furdozok[i].reszlegid == 0
+            for (int i = 0; i < furdozok.Count; i++)
+            {
+                if (furdozok[i].ora >= ora && furdozok[i].beki == 1 && furdozok[i].reszlegid == 0)
+                {
+                    ora = furdozok[i].ora;
+                }
+
+            }
+            for (int i = 0; i < furdozok.Count; i++)
+            {
+                if (furdozok[i].perc >= perc && furdozok[i].ora == ora && furdozok[i].beki == 1 && furdozok[i].reszlegid == 0)
+                {
+                    perc = furdozok[i].perc;
+                }
+
+            }
+            for (int i = 0; i < furdozok.Count; i++)
+            {
+                if (furdozok[i].mp >= mp && furdozok[i].perc == perc && furdozok[i].ora == ora && furdozok[i].beki == 1 && furdozok[i].reszlegid == 0)
+                {
+                    mp = furdozok[i].mp;
+                }
+
+            }
+            Console.WriteLine($"Az utolsó vendég {ora}:{perc}:{mp}-kor lépett ki az öltözőből.");
+
+            Console.WriteLine("\n3. feladat");
+            var egyreszenjart = 0;
+            var tomb = new int[1000];
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = 0;
+            }
+
+            for (int i = 0; i < furdozok.Count; i++)
+            {
+                var szemelyek = furdozok[i].id;
+                tomb[szemelyek]++;
+            }
+
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                if (tomb[i] > 0)
+                {
+                    if (tomb[i] == 4)
+                    {
+                        egyreszenjart++;
+                    }
+                }
+            }
+            Console.WriteLine($"A fürdőben {egyreszenjart} vendég járt csak egy részlegen./n");
+
+            Console.WriteLine("4. feladat");
+
+
+            var legtobbido = 0;
+            var legtobbId = 0;
+
+            var tomb2 = new int[1000];
+            for (int i = 0; i < tomb2.Length; i++)
+            {
+                tomb2[i] = 0;
+            }
+
+            var kezdes = 0;
+
+            for (int i = 0; i < furdozok.Count; i++)
+            {
+                var pillantnyiIdo = furdozok[i].ido;
+                var id = furdozok[i].id;
+                var test = kezdes;
+
+                if (furdozok[i].reszlegid == 0 && furdozok[i].beki == 1)
+                {
+                    kezdes = pillantnyiIdo;
+                }
+               
+                if (furdozok[i].reszlegid == 0 && furdozok[i].beki == 0)
+                {
+                    tomb2[id] += pillantnyiIdo - kezdes;
+                    kezdes = 0;
+                }
+
+            }
+
+            for (int i = tomb2.Length - 1; i >= 0; i--)
+            {
+                if (tomb2[i] > 0)
+                {
+                    if (tomb2[i] >= legtobbido)
+                    {
+                        legtobbido = tomb2[i];
+                        legtobbId = i;
+                    }
+                }
+            }
+            Console.WriteLine(legtobbId);
+
+            //for (int i = 0; i < furdozok.Count; i++)
+            //{
+            //    var szemely = furdozok[i];
+            //    for (int j = 0; j < furdozok.Count; j++)
+            //    {
+            //        if(szemely.id == furdozok[j].id)
+            //        {
+
+            //        }
+            //    }
+            //}
+        }
+    }
+}
