de7a2ed87fd15faecc2c44facbe8fe5c84c911ed
Joshua Hegedus <jhegedus9@gmail.com>
2022-05-17 23:11:12 +0200

    modositas

--- ellenorzo/index2.php
+++ ellenorzo/index2.php
@@ -7,7 +7,10 @@ $query = mysqli_query($connect, "SELECT * FROM urlapok WHERE id='$id'");
 while ($row = mysqli_fetch_assoc($query)){
     $id = $row['id'];
     $nev = $row['nev'];
-    $cim = $row['cim'];
+    $meret = $row['meret'];
+    $alap = $row['alap'];
+    $eros = $row['eros'];
+    $hagymas = $row['hagymas'];
 }
 
 ?>
@@ -15,22 +18,23 @@ while ($row = mysqli_fetch_assoc($query)){
 <html>
 <body>
 <form action="index3.php?id=<?php echo $id?>" method="post">
-    <input type="radio" name="radiocsoport" value="auto"> Auto <br>
-    <input type="radio" name="radiocsoport" value="motor"> Motor <br>
-    <input type="radio" name="radiocsoport" value="busz"> Busz <br>
-    <input type="radio" name="radiocsoport" value="gyalog"> Gyalog <br><br>
+    <!-- Fontos, hogy a csoport neve minden egy csoportba tartozo radionak ugyan az legyen   -->
+    <!-- Ezzel a csoport nevvel kell majd elerni az erteket   -->
+    <input type="text" name="nev">
 
-    <input type="checkbox" name="feltetel" value="fizet"> Elfogadom a felteteleket <br>
-    <input type="checkbox" name="hirlevel" value="nemfizet"> Feliratkozom a hirlevelre <br><br><br>
-
-    <select name="etkezes">
-        <option value="reggel">Reggel</option>
-        <option value="delben">Delben</option>
-        <option value="este">Este</option>
-        <option value="minden">Minden etkezes</option>
+    <select name="meret">
+        <option value="32">32</option>
+        <option value="45">45</option>
+        <option value="60">60</option>
     </select><br><br>
 
-    <textarea cols="30" rows="5" name="leiras"></textarea>
+    <input type="radio" name="alap" value="paradicsom" <?php if($alap == "paradicsom") echo "selected" ?>> Paradicsomos <br>
+    <input type="radio" name="alap" value="tejfol" <?php if($alap == "paradicsom") echo "selected" ?>> Tejfolos <br><br>
+
+    <textarea cols="30" rows="5" name="leiras"></textarea> <br>
+    <input type="checkbox" name="eros" value="eros"> Eros<br>
+    <input type="checkbox" name="hagymas" value="hagymas"> Hagymas <br><br><br>
+    <input type="submit" value="Modosit">
 </form>
 </body>
 </html>
--- ellenorzo/index3.php
+++ ellenorzo/index3.php
@@ -1,1 +1,14 @@
 <?php
+$id = $_GET['id'];
+$valasztottJarmu = $_POST['jarmu'];
+$valasztottEtkezes = $_POST['etkezes'];
+$leiras = $_POST['leiras'];
+
+// Checkboxok ellnorzese: bele kell tenni, egy isset-be ami ellenorzi, hogy ki van e pipazva
+if(isset($_POST['feltetel'])){
+    echo "Feltetel pipa";
+}
+
+if(isset($_POST['hirlevel'])){
+    echo "Hirlevel pipa";
+}
