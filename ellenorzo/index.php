e772d45347ac337c45cc8d23cffeecf4e1be1741
Joshua Hegedus <jhegedus9@gmail.com>
2022-05-17 23:17:05 +0200

    asd

--- ellenorzo/index.php
+++ ellenorzo/index.php
@@ -1,12 +1,19 @@
 <?php
-$connect = mysqli_connect("localhost", "root", "", "urlapok") or die("Ihh");
-$query = mysqli_query($connect, "SELECT * FROM urlapok");
+
+$connect = mysqli_connect("localhost", "root", "", "etterem") or die("Ihh");
+$query = mysqli_query($connect, "SELECT * FROM pizza");
 
 while ($row = mysqli_fetch_assoc($query)){
     $id = $row['id'];
     $nev = $row['nev'];
-    $cim = $row['cim'];
+    $meret = $row['meret'];
+    $alap = $row['alap'];
+    $leiras = $row['leiras'];
+    $eros = $row['eros'];
+    $hagymas = $row['hagymas'];
 
-    echo "<a href='index2.php?id=$id'>$id, $nev, $cim</a><br>";
-
+    echo "<a href='index2.php?id=$id'>$id, $nev, $meret, $alap, $leiras";
+    if($eros == "igen") echo " eros";
+    if($hagymas == "igen") echo " hagymas";
+    echo "</a>";
 }
